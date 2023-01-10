3889e068c4b23245f015f56b4d19358dd3684c9a
卫正明 <1904628089@qq.com>
2023-01-10 14:15:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ ### Introduce myself
+ 
+  
+ * 这个创意是参考国外一个牛人的，
+ * 我觉得很有趣，也用React+Hooks+TS重构了一下。
+ * 我放到[gitHub](https://github.com/way-jm/resume)上咯
+ * 如果你觉得我还不错，给Star哦！
---
+++ build/precache-manifest.3612c5d221baaffc037e1089b2d4d5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0e39c1ef8091ab813d0c114f85e3d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8db342b98954631f27b7",
+    "url": "./static/css/main.0feb8dae.chunk.css"
+  },
+  {
+    "revision": "365957e6fdfb13190f64",
+    "url": "./static/js/2.81d1ef4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.81d1ef4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db342b98954631f27b7",
+    "url": "./static/js/main.700dbc03.chunk.js"
+  },
+  {
+    "revision": "22d8bd3b4e68820dd5a2",
+    "url": "./static/js/runtime-main.d53f019b.js"
+  }
+]);
